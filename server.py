ab0badcd440ae029322aa913dde6fd71ca7f9f66
stiemannbscm <s.tiemann@basecom.de>
2025-06-11 14:09:12 +0200

    Update server.py
    Changed host and port

--- server.py
+++ server.py
@@ -99,4 +99,4 @@ def internal_error(e):
     return jsonify({'error': 'Internal server error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
